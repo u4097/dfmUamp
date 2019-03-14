036b391320fbe556275de031d1cce6efb96e6220
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-03-14 18:06:07 +0300

    fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,13 @@
+import com.android.build.gradle.api.ApplicationVariant
+import com.android.build.gradle.api.BaseVariantOutput
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+
 plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("android.extensions")
     id("com.github.ben-manes.versions") version "0.21.0" // uses gradle depUp ; show old dependencies in terminal
+    id("org.jmailen.kotlinter") version "1.21.0"
 }
 
 android {
@@ -19,6 +24,45 @@ android {
             setSourceCompatibility(1.8)
             setTargetCompatibility(1.8)
         }
+
+        sourceSets {
+            getByName("main").java.srcDirs("src/main/kotlin")
+            getByName("debug").java.srcDirs("src/debug/kotlin")
+            getByName("release").java.srcDirs("src/release/kotlin")
+        }
+
+        vectorDrawables.useSupportLibrary = true
+
+        androidExtensions {
+            isExperimental = true
+        }
+
+        versionCode =
+                (AppVersion.majorAppVersion * 10_000) + (AppVersion.minorAppVersion * 1_000) + (AppVersion.patchAppVersion * 100)
+
+        versionName = "${AppVersion.majorAppVersion}" +
+                ".${AppVersion.minorAppVersion}" +
+                ".${AppVersion.patchAppVersion}"
+
+
+        applicationVariants.all(object : Action<ApplicationVariant> {
+            override fun execute(variant: ApplicationVariant) {
+                println("variant: $variant")
+                variant.outputs.all(object : Action<BaseVariantOutput> {
+                    override fun execute(output: BaseVariantOutput) {
+
+                        val outputImpl = output as BaseVariantOutputImpl
+                        val fileName = output.outputFileName
+                                .replace("-release", "-release-v$versionName-vc$versionCode")
+                                .replace("-debug", "-debug-v$versionName-vc$versionCode")
+                        println("output file login: $fileName")
+                        outputImpl.outputFileName = fileName
+                    }
+                })
+            }
+        })
+
+
     }
     buildTypes {
         getByName("debug") {
@@ -32,6 +76,13 @@ android {
 
     }
 }
+
+kotlinter {
+    ignoreFailures = false
+    indentSize = 4
+    continuationIndentSize = 4
+}
+
 
 dependencies {
     implementation(project(":audio"))
@@ -58,4 +109,28 @@ dependencies {
 
     /** Glide */
     implementation(Libraries.glide)
+
+    // Picasso
+    implementation(Libraries.picasso)
+
+    /** Debug Drawer */
+    implementation(Libraries.debugDrawer)
+    implementation(Libraries.debugDrawerLeak)
+    implementation(Libraries.debugDrawerRetrofit)
+    implementation(Libraries.debugDrawerOkHTTP)
+    implementation(Libraries.debugDrawerTimber)
+
+    /** Logs */
+    // Timber
+    implementation(Libraries.timber)
+    // Chuck
+    debugImplementation(Libraries.chuck)
+    releaseImplementation(Libraries.chuckRelease)
+
+    /** LeakCanary */
+    debugImplementation(Libraries.leakCanaryAndroid)
+    releaseImplementation(Libraries.leakCanaryAndroidNoOp)
+
+    // Material Dialog
+    implementation(Libraries.materialDialog)
 }
